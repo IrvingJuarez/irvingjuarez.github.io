c6b91916621272456284ae9979f7604fa686eecd
Irving Juarez <irvingjuarez274@gmail.com>
2021-05-30 10:20:27 -0500

    Now the design had changed completely. The only two things left are the carousel, the cards design and the SEO stuff

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const headerButton = document.getElementById(`header-button`)
-const floatNav = document.getElementById(`header__float-nav`)
-const bar1 = document.getElementById(`bar1`), bar2 = document.getElementById(`bar2`), bar3 = document.getElementById(`bar3`)
+const PROJECT_CAROUSEL = document.getElementById("projects_carrousel")
+const CAROUSEL_CONTAINER = document.getElementById("carrousel_container")
+const CAROUSEL_CARD = document.getElementsByClassName("carrousel_item")
+const LEFT_ARROW = document.getElementById("left-arrow")
+const RIGHT_ARROW = document.getElementById("right-arrow")
+let scroll = 0, numberOfCard = 1
 
-function displayFloatNav() {
-    if(floatNav.style.display == "flex"){
-        floatNav.style.display = "none" 
-    }else{
-        floatNav.style.display = "flex"
+RIGHT_ARROW.addEventListener("click", () => {
+    numberOfCard++
+
+    scroll += PROJECT_CAROUSEL.scrollWidth / CAROUSEL_CARD.length
+    PROJECT_CAROUSEL.scroll(scroll, 0)
+
+    if(numberOfCard > 1){
+        LEFT_ARROW.style.display = "initial"
     }
-}
+
+    if(numberOfCard >= CAROUSEL_CARD.length){
+        RIGHT_ARROW.style.display = "none"
+    }
+})
+
+LEFT_ARROW.addEventListener("click", () => {
+    numberOfCard--
+
+    scroll -= PROJECT_CAROUSEL.scrollWidth / CAROUSEL_CARD.length
+    PROJECT_CAROUSEL.scroll(scroll, 0)
+
+    if(numberOfCard <= 1){
+        LEFT_ARROW.style.display = "none"
+    }
+
+    if(numberOfCard === CAROUSEL_CARD.length - 1){
+        RIGHT_ARROW.style.display = "initial"
+    }
+})
