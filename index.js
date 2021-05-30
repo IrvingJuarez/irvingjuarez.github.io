25de2e7d5c18952426dfaa713f4c2909faadf9b1
Irving Juarez <irvingjuarez274@gmail.com>
2021-05-30 17:45:19 -0500

    Refresh to my webpage

--- index.js
+++ index.js
@@ -3,34 +3,62 @@ const CAROUSEL_CONTAINER = document.getElementById("carrousel_container")
 const CAROUSEL_CARD = document.getElementsByClassName("carrousel_item")
 const LEFT_ARROW = document.getElementById("left-arrow")
 const RIGHT_ARROW = document.getElementById("right-arrow")
+const lengthOfMovement = PROJECT_CAROUSEL.scrollWidth * 0.164
 let scroll = 0, numberOfCard = 1
 
 RIGHT_ARROW.addEventListener("click", () => {
     numberOfCard++
 
-    scroll += PROJECT_CAROUSEL.scrollWidth / CAROUSEL_CARD.length
-    PROJECT_CAROUSEL.scroll(scroll, 0)
-
-    if(numberOfCard > 1){
-        LEFT_ARROW.style.display = "initial"
-    }
-
-    if(numberOfCard >= CAROUSEL_CARD.length){
-        RIGHT_ARROW.style.display = "none"
-    }
+    scroll += lengthOfMovement
+    movement(scroll, "Right")
 })
 
 LEFT_ARROW.addEventListener("click", () => {
     numberOfCard--
 
-    scroll -= PROJECT_CAROUSEL.scrollWidth / CAROUSEL_CARD.length
-    PROJECT_CAROUSEL.scroll(scroll, 0)
-
-    if(numberOfCard <= 1){
-        LEFT_ARROW.style.display = "none"
-    }
-
-    if(numberOfCard === CAROUSEL_CARD.length - 1){
-        RIGHT_ARROW.style.display = "initial"
-    }
+    scroll -= lengthOfMovement
+    movement(scroll, "Left")
 })
+
+function movement(flag, direction){
+    setTimeout(() => {
+        let movementValue
+
+        if(direction === "Right"){
+            movementValue = PROJECT_CAROUSEL.scrollLeft += 2
+
+            if(numberOfCard >= CAROUSEL_CARD.length){
+                if(movementValue < PROJECT_CAROUSEL.scrollWidth * 0.81408){
+                    PROJECT_CAROUSEL.scroll(movementValue, 0)
+                    movement(flag, "Right")
+                }else{
+                    RIGHT_ARROW.style.display = "none"
+                }
+            }else{
+                if(movementValue < flag){
+                    PROJECT_CAROUSEL.scroll(movementValue, 0)
+                    movement(flag, "Right")
+                }else{
+                    if(numberOfCard > 1){
+                        LEFT_ARROW.style.display = "initial"
+                    }
+                }
+            }
+        }else if(direction === "Left"){
+            movementValue = PROJECT_CAROUSEL.scrollLeft -= 2
+
+            if(movementValue <= flag){
+                if(numberOfCard <= 1){
+                    LEFT_ARROW.style.display = "none"
+                }
+            
+                if(numberOfCard === CAROUSEL_CARD.length - 1){
+                    RIGHT_ARROW.style.display = "initial"
+                }
+            }else{
+                PROJECT_CAROUSEL.scroll(movementValue, 0)
+                movement(flag, "Left")
+            }
+        }
+    }, 0)
+}
